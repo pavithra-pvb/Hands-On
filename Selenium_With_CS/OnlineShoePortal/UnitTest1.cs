f50751db42dad113d1ad80cb598108ba4e3f1c69
Pavithra Bhat <pavithra.premavb@gmail.com>
2024-04-19 17:29:23 +0530

    Selenium with CS - Practice

--- Selenium_With_CS/OnlineShoePortal/UnitTest1.cs
+++ Selenium_With_CS/OnlineShoePortal/UnitTest1.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Reflection;
 using System.Configuration;
 using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace OnlineShoePortal
 {
@@ -29,8 +30,23 @@ namespace OnlineShoePortal
              */
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.FindElement(By.LinkText("Sign In Portal")).Click();
-            
+
+            var txtbx_userlength = driver.FindElements(By.Id("usr")).Count;
+            Assert.AreEqual(1, txtbx_userlength);
+
+            var txtbx_pwdlength = driver.FindElements(By.Id("pwd")).Count;
+            Assert.AreEqual(1, txtbx_pwdlength);
+
+            var loginButton = driver.FindElements(By.XPath("//input[@value='Login']"));
+            Assert.AreEqual(1, loginButton);
+
             driver.FindElement(By.Id("NewRegistration")).Click();
+
+            SelectElement drpSalutation = new SelectElement(driver.FindElement(By.Id("Salutation")));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            drpSalutation.SelectByText("Ms.");
+            
+            
            
         }
     }
